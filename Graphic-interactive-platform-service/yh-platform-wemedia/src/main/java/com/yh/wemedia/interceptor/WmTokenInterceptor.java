fa5352343c6a57bfdf7442602cdaa54034778b5f
yh <2827902614@qq.com>
2023-08-02 13:04:17 +0800

    Realize the login function of we media terminal
    1. Create the corresponding database and use mybatis-plus to manage it
    
    2. Register the gateway and its own microservices on nacos, and configure them accordingly
    
    3. Reverse proxy with nginx
    
    实现自媒体端登录功能
    
    1.创建对应数据库，使用mybatis-plus进行管理
    
    2.在nacos上注册网关与自身微服务，并进行对应配置
    
    3.使用nginx进行反向代理

--- Graphic-interactive-platform-service/yh-platform-wemedia/src/main/java/com/yh/wemedia/interceptor/WmTokenInterceptor.java
+++ Graphic-interactive-platform-service/yh-platform-wemedia/src/main/java/com/yh/wemedia/interceptor/WmTokenInterceptor.java
@@ -8,11 +8,13 @@ import javax.servlet.http.HttpServletResponse;
 
 public class WmTokenInterceptor implements HandlerInterceptor {
 
+   /* 得到header中的用户信息，并存到当前线程中*/
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         return HandlerInterceptor.super.preHandle(request, response, handler);
     }
+
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
